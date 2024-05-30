e05c8f177446064a0cc70f61a3742fc1314d703d
I Putu Wira Pratama Putra <92298020+wirapratamaz185@users.noreply.github.com>
2024-05-30 03:11:32 +0000

    fix auth content type

--- utils/interface/auth.ts
+++ utils/interface/auth.ts
@@ -2,31 +2,32 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export interface User {
-    id: string;
-    email: string;
-    password: string;
-    username: string;
-    imageUrl: string;
+  id: string;
+  email: string;
+  password?: string;
+  username: string;
+  imageUrl?: string;
 }
 
 interface LoginRequestBody {
-    email: string;
-    password: string;   
+  email: string;
+  password: string;
 }
 
 export interface LoginRequest extends NextApiRequest {
-    body: LoginRequestBody;
+  body: LoginRequestBody;
 }
 
 export interface UserProfile {
-    id: string;
-    email: string;
-    username: string;
-    bio: string;
-    imageUrl: string;
+  id: string;
+  email: string;
+  username: string;
+  bio: string;
+  imageUrl: string;
 }
 
 export interface AuthContextType {
-    user: User | null;
-    setUser: (user: User | null) => void;
+  user: User | null;
+  setUser: (user: User | null) => void;
+  id: string | undefined;
 }
